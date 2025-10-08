406b293d59f53f2f74f5d2556907fc167a7465cf
Abhinav K <abhinavksathyann@gmail.com>
2025-10-08 18:47:21 +0530

    modified:   AboutPage.java

--- AboutPage.java
+++ AboutPage.java
@@ -1,3 +1,34 @@
-public class AboutPage {
-    
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class AboutPage extends JFrame {
+    public AboutPage() {
+        setTitle("About This App");
+        setSize(400, 250);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        JTextArea text = new JTextArea("""
+                Advanced Java Calculator
+                ------------------------
+                Created by: Abhinav K
+                Technologies: Java Swing, OOP
+                Features: Basic + Scientific Calculations
+                Version: 2.0
+                """);
+        text.setEditable(false);
+        text.setFont(new Font("Monospaced", Font.PLAIN, 14));
+        add(text, BorderLayout.CENTER);
+
+        JButton back = new JButton("Back");
+        back.addActionListener(e -> {
+            dispose();
+            new HomePage();
+        });
+        add(back, BorderLayout.SOUTH);
+
+        setVisible(true);
+    }
 }
